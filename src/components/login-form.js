2529aec5e11acb12f8a0b2866449d7e8455dde2a
Timothy <superfluous11@gmail.com>
2019-01-05 05:34:05 -0800

    creates login and registration forms

---
+++ src/components/login-form.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Field, reduxForm, focus, clearSubmitErrors } from 'redux-form';
+import Input from './input';
+import { login } from '../actions/auth';
+
+export class LoginForm extends React.Component {
+  onSubmit(values) {
+    return this.props.dispatch(login(values.username, values.password));
+  }
+
+  render() {
+    const error = this.props.error && (
+      <div className="form-error" aria-live="polite">
+        {this.props.error}
+      </div>
+    );
+
+    return (
+      <>
+        {error}
+        <form
+          className="login-form"
+          onSubmit={this.props.handleSubmit(values =>
+            this.onSubmit(values)
+          )}>
+          <label htmlFor="username">Username</label>
+          <Field
+            component={Input}
+            type="text"
+            name="username"
+            id="username"
+          />
+          <label htmlFor="password">Password</label>
+          <Field
+            component={Input}
+            type="password"
+            name="password"
+            id="password"
+          />
+          <button disabled={this.props.pristine || this.props.submitting}>
+            Login
+          </button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'login',
+  onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username')),
+  onChange: (values, dispatch, props) => {
+    if (props.error) dispatch(clearSubmitErrors('login'));
+  }
+})(LoginForm);
---
+++ src/components/registration-form.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Field, reduxForm, focus, clearSubmitErrors } from 'redux-form';
+import { registerUser } from '../actions/users';
+import { login } from '../actions/auth';
+import Input from './input';
+
+export class RegistrationForm extends React.Component {
+  onSubmit(values) {
+    const { username, password } = values;
+    const user = { username, password };
+    return this.props
+      .dispatch(registerUser(user))
+      .then(() => this.props.dispatch(login(username, password)));
+  }
+
+  render() {
+    const error = this.props.error && (
+      <div className="form-error" aria-live="polite">
+        {this.props.error}
+      </div>
+    );
+
+    return (
+      <>
+        {error}
+        <form
+          className="registration-form"
+          onSubmit={this.props.handleSubmit(values =>
+            this.onSubmit(values)
+          )}>
+          <label htmlFor="username">Username</label>
+          <Field
+            component={Input}
+            type="text"
+            name="username"
+          />
+          <label htmlFor="password">Password</label>
+          <Field
+            component={Input}
+            type="password"
+            name="password"
+          />
+          <button
+            type="submit"
+            disabled={this.props.pristine || this.props.submitting}
+          >
+            Register
+          </button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'registration',
+  onSubmitFail: (errors, dispatch) =>
+    dispatch(focus('registration', Object.keys(errors)[0])),
+  onChange: (values, dispatch, props) => {
+    if (props.error) dispatch(clearSubmitErrors('registration'));
+  }
+})(RegistrationForm);
