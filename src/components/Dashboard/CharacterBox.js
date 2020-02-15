d46700654579a6016452a1f6843d902ec7f6f611
Timothy <timothychang@tutanota.com>
2020-02-15 02:43:39 -0800

    Rewrite Feedback, Error and CharacterBox components

--- src/components/Dashboard/CharacterBox.js
+++ src/components/Dashboard/CharacterBox.js
@@ -1,39 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Loading from '../Loading/Loading';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-class CharacterBox extends React.Component {
-	static propTypes = {
-		character: PropTypes.string.isRequired,
-		loading: PropTypes.bool.isRequired,
-		system: PropTypes.string.isRequired
-	};
+export default () => {
+	const character = useSelector(state => state.currentQuestion.character);
+	const loading = useSelector(state => state.currentQuestion.loading);
+	const system = useSelector(state => state.currentQuestion.system);
 
-	render = () => {
-		const { character, loading, system } = this.props;
-
-		return (
-			<>
-				<div className='question'>
-					<h3>What is the romaji for this <span style={{color: '#e44d4d'}}>{system}</span> character?</h3>
-				</div>
-				<div className="character-box">
-					<h2 className='character'>{ loading ? <Loading margin={'px'} /> : character}</h2>
-				</div>
-			</>
-		);
-  }
+	return (
+		<>
+			<div className='question'>
+				<h3>What is the romaji for this <span style={{color: '#e44d4d'}}>{system}</span> character?</h3>
+			</div>
+			<div className="character-box">
+				<h2 className='character'>{ loading ? <Loading margin={'px'} /> : character}</h2>
+			</div>
+		</>
+	);
 }
-
-const mapStateToProps = (state) => {
-	const { character, loading, system } = state.currentQuestion;
-	
-	return {
-		character,
-		loading,
-		system
-	};
-};
-
-export default connect(mapStateToProps)(CharacterBox);
